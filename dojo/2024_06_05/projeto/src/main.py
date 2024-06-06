e5cd1b627519314521ed0c1214850f70d9978dae
rapdos-s <raphael.santos.esteves@gmail.com>
2024-06-06 18:05:05 -0300

    translation

--- dojo/2024_06_05/projeto/src/main.py
+++ dojo/2024_06_05/projeto/src/main.py
@@ -1,119 +1,122 @@
 import sqlite3
 import random
 
-def executa_teste(testes, conn):
-    for index, teste in enumerate(testes):
-        retorno = teste["funcao"](conn)
-        if retorno == teste["valor_esperado"]:
-            print(f"🟢 [Teste {index} OK] {teste["enunciado"]}")
+projects = [
+    {"name": "libft", "phase": 1},
+    {"name": "get_next_line", "phase": 1},
+    {"name": "ft_printf", "phase": 1},
+    {"name": "Born2beroot", "phase": 1},
+    {"name": "fdf", "phase": 1},
+    {"name": "fractol", "phase": 1},
+    {"name": "so_long", "phase": 1},
+    {"name": "minitalk", "phase": 1},
+    {"name": "pipex", "phase": 1},
+    {"name": "push_swap", "phase": 2},
+    {"name": "minishell", "phase": 2},
+    {"name": "Philosophers", "phase": 2},
+    {"name": "NetPractice", "phase": 2},
+    {"name": "cub3d", "phase": 2},
+    {"name": "mini_RT", "phase": 2},
+    {"name": "CPP Module 00", "phase": 3},
+    {"name": "CPP Module 01", "phase": 3},
+    {"name": "CPP Module 02", "phase": 3},
+    {"name": "CPP Module 03", "phase": 3},
+    {"name": "CPP Module 04", "phase": 3},
+    {"name": "CPP Module 05", "phase": 3},
+    {"name": "CPP Module 06", "phase": 3},
+    {"name": "CPP Module 07", "phase": 3},
+    {"name": "CPP Module 08", "phase": 3},
+    {"name": "CPP Module 09", "phase": 3},
+    {"name": "Inception", "phase": 3},
+    {"name": "webserv", "phase": 3},
+    {"name": "ft_irc", "phase": 3},
+    {"name": "ft_transcendence", "phase": 3},
+]
+
+
+def run_tests(tests, conn):
+    for index, test in enumerate(tests):
+        result = test["function"](conn)
+        if result == test["expected_value"]:
+            print(f"🟢 [Test {index} OK] {test['description']}")
         else:
-            print(f"🔴 [Teste {index} KO] {teste["enunciado"]}")
+            print(f"🔴 [Test {index} KO] {test['description']}")
+            print(f"\tresult        : {result}")
+            print(f"\texpected value: {test['expected_value']}")
 
 
-def cria_banco():
+def create_database():
     db_name = "/app/volumes/db_name"
     conn = sqlite3.connect(db_name)
 
     return conn
 
 
-def cria_tabela_cadetes(conn):
+def create_tables(conn):
     cur = conn.cursor()
 
-    cur.execute('''CREATE TABLE IF NOT EXISTS cadetes(
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS cadets(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         login TEXT NOT NULL,
-        id_projeto INTEGER NOT NULL,
-        FOREIGN KEY (id_projeto) REFERENCES projetos(id)
-        )''')
+        id_projects INTEGER NOT NULL,
+        FOREIGN KEY (id_projects) REFERENCES projects(id)
+        )"""
+    )
 
-    #teste = cur.execute("SELECT * FROM cadetes")
-    #print(f">>>>>>>>>{teste.description}")
-    # tabela_cadetes_tupla.fetchall()
-    # tabela_cadetes_list = []
-
-    # for table in tabela_cadetes_tupla:
-    #     tabela_cadetes_list.append(table[0])
-    # return tabela_cadetes_list
-
-
-def cria_tabelas(conn):
-    cur = conn.cursor()
-
-    cur.execute('''CREATE TABLE IF NOT EXISTS projetos(
+    cur.execute(
+        """CREATE TABLE IF NOT EXISTS projects(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT NOT NULL,
-        fase INTEGER NOT NULL
-        )''')
-    teste = cur.execute("SELECT * FROM projetos")
-    print(f">>>>>>>>>{teste.description}")
-    
-    print(f"🤖: {cria_tabela_cadetes(conn)}")
+        phase INTEGER NOT NULL
+        )"""
+    )
 
 
-def popula_tabela_cadetes(conn):
+def populate_cadets_table(conn):
     cur = conn.cursor()
-    logins = ["dmoreira", "lumedeir", "adantas-", "carrodri", "csantos-", "csouza-f", "fnacarel", "kdaiane-", "lade-lim", "lamorim", "luizedua", "mamacedo", "namoreir", "pdavi-al", "r-leite", "rabustam", "rapdos-s", "rede-and", "telias-p", "vcedraz-"]
-
-    lista_indices = [ index for index, projeto in enumerate(projetos)]
-    print(lista_indices)
-    for login in logins:
-        id_projeto = random.choice(lista_indices)
-        cur.execute('INSERT INTO cadetes (login, id_projeto) VALUES (?, ?)', (login, id_projeto))
-    print(f"PEGASUS: {cur.execute(f'''SELECT * FROM cadetes''').fetchall()}")
-
-
-
-    # cur.execute(f'''INSERT INTO cadetes(login, id_projeto) VALUES('joao', '42')''')
-      # cur.execute(f'''INSERT INTO projetos(name, fase) VALUES('{projeto["nome"], projeto["fase"]}')''')
-
-
-
-projetos = [
-    {"nome": "libft", "fase": 1},
-    {"nome": "get_next_line", "fase": 1},
-    {"nome": "ft_printf", "fase": 1},
-    {"nome": "Born2beroot", "fase": 1},
-    {"nome": "fdf", "fase": 1},
-    {"nome": "fractol", "fase": 1},
-    {"nome": "so_long", "fase": 1},
-    {"nome": "minitalk", "fase": 1},
-    {"nome": "pipex", "fase": 1},
-    {"nome": "push_swap", "fase": 2},
-    {"nome": "minishell", "fase": 2},
-    {"nome": "Philosophers", "fase": 2},
-    {"nome": "NetPractice", "fase": 2},
-    {"nome": "cub3d", "fase": 2},
-    {"nome": "mini_RT", "fase": 2},
-    {"nome": "CPP Module 00", "fase": 3},
-    {"nome": "CPP Module 01", "fase": 3},
-    {"nome": "CPP Module 02", "fase": 3},
-    {"nome": "CPP Module 03", "fase": 3},
-    {"nome": "CPP Module 04", "fase": 3},
-    {"nome": "CPP Module 05", "fase": 3},
-    {"nome": "CPP Module 06", "fase": 3},
-    {"nome": "CPP Module 07", "fase": 3},
-    {"nome": "CPP Module 08", "fase": 3},
-    {"nome": "CPP Module 09", "fase": 3},
-    {"nome": "Inception", "fase": 3},
-    {"nome": "webserv", "fase": 3},
-    {"nome": "ft_irc", "fase": 3},
-    {"nome": "ft_transcendence", "fase": 3}
+    logins = [
+        "dmoreira",
+        "lumedeir",
+        "adantas-",
+        "carrodri",
+        "csantos-",
+        "csouza-f",
+        "fnacarel",
+        "kdaiane-",
+        "lade-lim",
+        "lamorim",
+        "luizedua",
+        "mamacedo",
+        "namoreir",
+        "pdavi-al",
+        "r-leite",
+        "rabustam",
+        "rapdos-s",
+        "rede-and",
+        "telias-p",
+        "vcedraz-",
     ]
 
-def popula_tabela_projetos(conn, name):
+    project_ids = [index for index, project in enumerate(projects)]
+    for login in logins:
+        project_id = random.choice(project_ids)
+        cur.execute('INSERT INTO cadets (login, id_projects) VALUES (?, ?)', (login, project_id))
+
+
+def populate_projects_table(conn):
     cur = conn.cursor()
 
-    
-
-    for projeto in projetos:
-        cur.execute(f'''INSERT INTO projetos(name, fase) VALUES('{projeto["nome"], projeto["fase"]}')''')
+    for project in projects:
+        cur.execute('INSERT INTO projects (name, phase) VALUES (?, ?)', (project["name"], project["phase"]))
 
 
-def pega_tabelas(conn):
+def get_table(conn):
     cur = conn.cursor()
 
-    test = cur.execute("SELECT name FROM sqlite_master")
+    test = cur.execute(
+        "SELECT name FROM sqlite_master WHERE type='table' AND name != 'sqlite_sequence'"
+    )
     tables_tupla = cur.fetchall()
     tables_list = []
 
@@ -124,21 +127,21 @@ def pega_tabelas(conn):
 
 if __name__ == "__main__":
     testes = [
-    {
-        "funcao": pega_tabelas,
-        "valor_esperado": ['cadetes', 'projetos'],
-        "enunciado": "Verifica o nome das tabelas"
-    },
-    {
-        "funcao": pega_tabelas,
-        "valor_esperado": ['foo', 'bar'],
-        "enunciado": "Isso aqui vai dar erro"
-    }
+        {
+            "function": get_table,
+            "expected_value": ["foo", "bar"],
+            "description": "This must be an error",
+        },
+        {
+            "function": get_table,
+            "expected_value": ["cadets", "projects"],
+            "description": "Check database's tables created",
+        },
     ]
-    conexao_banco = cria_banco()
+    database_connection = create_database()
 
-    cria_tabelas(conexao_banco)
-    # print(pega_tabelas(conexao_banco))
+    create_tables(database_connection)
 
-    executa_teste(testes, conexao_banco)
-    popula_tabela_cadetes(conexao_banco)
+    run_tests(testes, database_connection)
+    populate_cadets_table(database_connection)
+    populate_projects_table(database_connection)
